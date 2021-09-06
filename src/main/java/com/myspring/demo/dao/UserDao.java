98bed6be6294273bc6434c0360ef8990cf413693
ZhangXD <1296753187@qq.com>
2021-09-06 10:21:48 +0800

    bug fix

---
+++ src/main/java/com/myspring/demo/dao/UserDao.java
@@ -0,0 +1,6 @@
+package com.myspring.demo.dao;/**
+ * @Author ZhangXD
+ * @Date 2021/9/6 9:50
+ * @Description 
+ */public class UserDao {
+}
---
+++ src/main/java/com/myspring/demo/entity/po/User.java
@@ -0,0 +1,26 @@
+package com.myspring.demo.dao;
+
+import lombok.Data;
+
+/**
+ * @Author ZhangXD
+ * @Date 2021/9/6 9:43
+ * @Description
+ */
+
+@Data
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String address;
+
+    private String email;
+
+    private String phone;
+
+}
